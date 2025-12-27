75fd25304dc34354c88f1460fb77b7925bbe3259
kitoumasaomi <kanfutrooper@gmail.com>
2025-12-27 16:48:38 +0900

    BeanModel.java を作成

--- BeanModel.java
+++ BeanModel.java
@@ -1,5 +1,8 @@
 import javax.faces.bean.ManagedBean;
 import javax.annotation.PostConstruct;
+import org.primefaces.model.menu.MenuModel;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultMenuItem;
 
 @ManagedBean(name = "mb")
 public class BeanModel {
