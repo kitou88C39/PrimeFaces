88e0ded7e7bcadb4b5e107bbaa0543153043e996
kitoumasaomi <kanfutrooper@gmail.com>
2025-12-27 14:26:56 +0900

    SelectOneRadio.java を作成

---
+++ SelectOneRadio.java
@@ -0,0 +1,13 @@
+@ManagedBean(name = "lang")
+public class SelectOneRadio {
+
+    String Language;
+
+    public String getLanguage() {
+        return Language;
+    }
+
+    public void setLanguage(String language) {
+        this.Language = language;
+    }
+}
