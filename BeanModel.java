980affd6047cad93cbebcd1fec6db99ed6f0792c
kitoumasaomi <kanfutrooper@gmail.com>
2025-12-27 15:39:21 +0900

    BreadCrumbMenu.java を作成

---
+++ BeanModel.java
@@ -0,0 +1,28 @@
+@ManagedBean(name = "mb")
+public class BeanModel {
+
+    public MenuModel menuModel;
+
+    @PostConstruct
+    public void init() {
+
+        menuModel = new DefaultMenuModel();
+        DefaultMenuItem menuItem = new DefaultMenuItem();
+        menuItem.setValue("Home");
+        menuItem.setUrl("https:///www.");
+        menuModel.addElement(menuItem);
+
+        DefaultMenuItem menuItem1 = new DefaultMenuItem();
+        menuItem1.setValue("JSF");
+        menuItem1.setUrl("https:///www.");
+        menuModel.addElement(menuItem1);
+    }
+
+    public MenuModel getMenuModel() {
+        return menuModel;
+    }
+
+    public void setMenuModel(MenuModel menuModel) {
+        this.menuModel = menuModel;
+    }
+}
