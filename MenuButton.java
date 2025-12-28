b0b974cfedee883787c3ea79f0dc5fd808e3c36b
kitoumasaomi <kanfutrooper@gmail.com>
2025-12-28 21:29:46 +0900

    MenuButton.java を作成

---
+++ MenuButton.java
@@ -0,0 +1,20 @@
+@ManagedBean(name = "mb")
+public class MenuButton {
+
+    public void file() {
+        addMessage("File", "File clicked");
+    }
+
+    public void edit() {
+        addMessage("Edit", "Edit clicked");
+    }
+
+    public void view() {
+        addMessage("View", "View clicked");
+    }
+
+    public void addMessage(String summary, String detail) {
+        FaceMessage message = new FaceMessage(FaceMessage.SEVERITY_INFO, summary, detail);
+        FaceContext.getCurrentInstance().addMessage(null, message);
+    }
+}
