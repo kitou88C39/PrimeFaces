dfcbc3be16f08b457c3305523be9bcde048eeaf3
kitoumasaomi <kanfutrooper@gmail.com>
2025-12-28 20:38:57 +0900

    MegaMenu.java を作成

---
+++ MegaMenu.java
@@ -0,0 +1,13 @@
+@ManagedBean(name = "mb")
+public class BeanModel {
+
+    private String crientation = "horizontal";
+
+    public String getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(String orientation) {
+        this.orientation = orientation;
+    }
+}
