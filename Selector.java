1dc3304e2ea50398f54a99c2c3531926d161e462
kitoumasaomi <kanfutrooper@gmail.com>
2025-12-29 21:26:42 +0900

    Selector.html を作成

---
+++ Selector.java
@@ -0,0 +1,31 @@
+@ManagedBean(name = "sb")
+public class SelectorBean {
+
+    private String firstName;
+    private String lastName;
+    private String gender;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.fgender = gender;
+    }
+}
