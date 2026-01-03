d37f92a8b1056bcbe77c240e71dcd225edf30b6a
kitoumasaomi <kanfutrooper@gmail.com>
2026-01-03 20:38:36 +0900

    Inplace.java を作成

---
+++ InplaceBean.java
@@ -0,0 +1,13 @@
+@ManagedBean(name = "ib")
+public class InplaceBean {
+
+    private String display = "Ripples Code";
+
+    public String getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(String display) {
+        this.display = display;
+    }
+}
