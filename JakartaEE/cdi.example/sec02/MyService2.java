54751ae3dcb66464e2640217f4f41df58c4ed3c9
kitoumasaomi <kanfutrooper@gmail.com>
2026-01-05 20:45:18 +0900

    MyService3.javaを作成

--- JakartaEE/cdi.example/sec02/MyService2.java
+++ JakartaEE/cdi.example/sec02/MyService2.java
@@ -1,5 +1,10 @@
 package JakartaEE.cdi.example.sec02;
 
-public class MyService2 {
+public class MyService2 implements MyService1 MyService
+{
 
+    @Override
+    public String getMessage() {
+        return "Hello World";
+    }
 }
