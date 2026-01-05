e2857d6a80db60250b65cd70f9f0a3694d1a9818
kitoumasaomi <kanfutrooper@gmail.com>
2026-01-05 20:32:26 +0900

    CDIApp.java を作成

--- JakartaEE/cdi.example/sec02/CDIApp.java
+++ JakartaEE/cdi.example/sec02/CDIApp.java
@@ -1,5 +1,15 @@
 package JakartaEE.cdi.example.sec02;
 
+import jakarta.enterprise.context.Dependent;
+
+@Dependent
 public class CDIApp {
 
+    @Inject
+    private MyService myService;
+
+    public void run() {
+        System.out.println("Hello CDIApp sec02");
+        System.out.println(this.myService.getMessage());
+    }
 }
