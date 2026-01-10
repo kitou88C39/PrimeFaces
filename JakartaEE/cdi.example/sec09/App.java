00ca127a625ca51fc5f1fc7c448f4c99e85a5c45
kitoumasaomi <kanfutrooper@gmail.com>
2026-01-10 20:40:04 +0900

    App.java を作成

---
+++ JakartaEE/cdi.example/sec09/App.java
@@ -0,0 +1,13 @@
+package JakartaEE.cdi.example.sec09;
+
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+
+public class App {
+    public static void main(String[] args) {
+        Weld weld = new Weld();
+        try (WeldContainer container = weld.initialize()) {
+            container.select(CDIApp.class).get().run();
+        }
+    }
+}
