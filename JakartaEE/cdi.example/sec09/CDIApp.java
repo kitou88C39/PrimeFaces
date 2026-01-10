73fdc7547ea647fe1e2a832afddc47822fd718bb
kitoumasaomi <kanfutrooper@gmail.com>
2026-01-10 20:44:22 +0900

    CDIApp.java を作成

--- JakartaEE/cdi.example/sec09/CDIApp.java
+++ JakartaEE/cdi.example/sec09/CDIApp.java
@@ -13,19 +13,9 @@ public class CDIApp {
     private MyService myService3;
 
     public void run() {
-        System.out.println("Hello CDIApp sec08");
+        System.out.println("Hello CDIApp sec09");
         System.out.println("this.myService1.getMessage()");
         System.out.println("this.myService2.getMessage()");
         System.out.println("this.myService3.getMessage()");
-    }
-
-    @PostConstruct
-    public void postConstruct() {
-        System.err.println("CDIApp sec08 @PostConstruct");
-    }
-
-    @PreDestory
-    public void preDestory() {
-        System.err.println("CDIApp sec08 @PreDestory");
     }
 }
