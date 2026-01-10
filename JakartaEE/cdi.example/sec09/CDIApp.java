72f9e80445c484c91b09ea024f76f8ec5692b439
kitoumasaomi <kanfutrooper@gmail.com>
2026-01-10 21:11:23 +0900

    CDIApp.java を作成

--- JakartaEE/cdi.example/sec09/CDIApp.java
+++ JakartaEE/cdi.example/sec09/CDIApp.java
@@ -1,21 +1,14 @@
 package JakartaEE.cdi.example.sec09;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestory;
+import jakarta.enterprise.event.Event;
 import jakarta.inject.Inject;
 
 public class CDIApp {
     @Inject
-    private MyService myService1;
-    @Inject
-    private MyService myService2;
-    @Inject
-    private MyService myService3;
+    private Event<MyService> event;
 
     public void run() {
         System.out.println("Hello CDIApp sec09");
-        System.out.println("this.myService1.getMessage()");
-        System.out.println("this.myService2.getMessage()");
-        System.out.println("this.myService3.getMessage()");
+        this.event.fire(new MyEvent("My event fired!"));
     }
 }
