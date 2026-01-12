378ba3930c8d75a3be22e9710abc0731025d23f3
kitoumasaomi <kanfutrooper@gmail.com>
2026-01-12 11:56:31 +0900

    MyService2.javaを作成

---
+++ JakartaEE/interceptor.example/sec02/MyService2.java
@@ -0,0 +1,16 @@
+package JakartaEE.interceptor.example.sec02;
+
+import jakarta.interceptor.AroundInvoke;
+import jakarta.interceptor.Interceptor;
+import jakarta.interceptor.InvocationContext;
+
+@Interceptor
+public class MyService2 {
+    @AroundInvoke
+    public Object intercept(InvocationContext context) throws Exception {
+        System.out.println("MyInterceptor2 - before invocation");
+        Object result = context.proceed();
+        System.out.println("MyInterceptor2 - after invocation");
+        return result;
+    }
+}
