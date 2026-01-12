cb846fcee22d0c45fb8573e9719acfe97cfd786f
kitoumasaomi <kanfutrooper@gmail.com>
2026-01-12 15:38:39 +0900

    MyInterceptor1.javaを作成

---
+++ JakartaEE/interceptor.example/sec04/MyService1.java
@@ -0,0 +1,18 @@
+package JakartaEE.interceptor.example.sec04;
+
+import jakarta.interceptor.AroundInvoke;
+import jakarta.interceptor.Interceptor;
+import jakarta.interceptor.InvocationContext;
+
+@MyBinding
+@Interceptor
+@Priority(Intercepter.Priority.APPLICATION + 1)
+public class MyService1 {
+    @AroundInvoke
+    public Object aroundInvoke(InvocationContext context) throws Exception {
+        System.out.println("MyInterceptor1 - before proceed");
+        Object result = context.proceed();
+        System.out.println("MyInterceptor1 - after proceed");
+        return result;
+    }
+}
