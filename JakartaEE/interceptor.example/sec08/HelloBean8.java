f15c7c50384604ca4a06fec19dea90391dbc3cda
kitoumasaomi <kanfutrooper@gmail.com>
2026-01-17 10:47:16 +0900

    HelloBean8.javaを作成

--- JakartaEE/interceptor.example/sec08/HelloBean8.java
+++ JakartaEE/interceptor.example/sec08/HelloBean8.java
@@ -1,9 +1,14 @@
 package JakartaEE.interceptor.example.sec08;
 
 import jakarta.ejb.Stateless;
+import jakarta.ejb.EJB;
+import ui.HelloBean;
 
 @Stateless()
 public class HelloBean8 {
+    @EJB
+    private HelloBean hellobean;
+
     public void sayHello(String name) {
         System.out.println("Hello " + name + "!");
     }
