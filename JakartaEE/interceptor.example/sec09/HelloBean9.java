9547035d31f5ee9bced6ada1ea1f698e9862df0a
kitoumasaomi <kanfutrooper@gmail.com>
2026-01-17 11:29:17 +0900

    HelloBean9.javaを作成

--- JakartaEE/interceptor.example/sec09/HelloBean9.java
+++ JakartaEE/interceptor.example/sec09/HelloBean9.java
@@ -1,5 +1,22 @@
 package JakartaEE.interceptor.example.sec09;
 
+import jakarta.ejb.Stateless;
+import jakarta.ejb.EJB;
+import ui.HelloBean;
+import jakarta.interceptor.Interceptors;
+
+@Stateless()
+@Interceptors({ MyInterceptor.class })
 public class HelloBean9 {
+    @EJB
+    private HelloBean hellobean;
+
+    public void sayHello(String name) {
+        System.out.println("Hello " + name + "!");
+        this.hellobean.sayHello("@EJB");
+    }
+
+    public void sayHello(Timer timer) {
+        System.out.println("Timeout9 method executed");
 
 }
